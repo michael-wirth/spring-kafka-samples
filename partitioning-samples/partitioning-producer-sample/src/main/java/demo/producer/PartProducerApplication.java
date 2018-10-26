39a9954077c82e5ba1b2d180895fa684712521db
Michael Wirth <michael.wirth@mimacom.com>
2018-10-26 11:36:52 +0200

    initial commit

---
+++ partitioning-samples/partitioning-producer-sample/src/main/java/demo/producer/PartProducerApplication.java
@@ -0,0 +1,13 @@
+package demo.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartProducerApplication.class, args);
+	}
+
+}
